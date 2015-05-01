018d8d4e4f33cc435044c1ffe33161f28158640f
Jean-Louis MOINY <jlmoiny@outlook.be>
2015-05-01 13:56:44 +0200

    Enumes Days and Services added

---
+++ RestoBook.Model/Enums/Constants.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoBook.Common.Model.Enums
+{
+    public class Constants
+    {
+        public enum Days
+        {
+            Monday = 1,
+            Thuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        public enum Services
+        {
+            Midday=1,
+            Evening
+        }
+    }
+}
